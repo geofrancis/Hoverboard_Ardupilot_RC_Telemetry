471c456bb1ded4bf957d201d64e6bd5ad55d9489
geofrancis <o.o.o.o@hotmail.co.uk>
2024-06-30 23:49:33 +0100

    Update hoverboard_telemetry_2040.ino

--- hoverboard_telemetry_2040.ino
+++ hoverboard_telemetry_2040.ino
@@ -1,152 +1,90 @@
 
-#include "mavlink/common/mavlink.h"
-#include "mavlink/common/mavlink_msg_servo_output_raw.h"
 
-SerialPIO HoverSerial1(2, 3);
-SerialPIO HoverSerial2(4, 5);
-SerialPIO HoverSerial3(6, 7);
-//SerialPIO ser6(10,11);
+void power() {
 
+  if (powerchannel > 1800) {
+    brdall = 1;
+  }
 
+  if (powerchannel < 1300) {
+    brdall = 0;
+  }
 
-
-int MAXRPM = 200;
-
-int powerchannel = 1000;
-
-const int board1power = 8;
-const int board2power = 9;
-const int board3power = 10;
-
-const int board1switch = 11;
-const int board2switch = 12;
-const int board3switch = 13;
-
-
-
-#define START_FRAME 0xABCD  // [-] Start frme definition for reliable serial communication
-//#define DEBUG_RX            // [-] Debug received data. Prints all bytes to serial (comment-out to disable)
-
-int leftoutput1 = 0;
-int rightoutput1 = 0;
-int leftoutput2 = 0;
-int rightoutput2 = 0;
-int leftoutput3 = 0;
-int rightoutput3 = 0;
-//low voltage
-//speed limit
-//power control
-
-int boardson = 0;
-int board1on = 0;
-int board2on = 0;
-int board3on = 0;
-
-int board1error = 0;
-int board2error = 0;
-int board3error = 0;
-
-int startupok = 0;
-int startcycle1 = 0;
-int startcycle2 = 0;
-int startcycle3 = 0;
-int stopcycle = 0;
-
-unsigned long buttonMillis = 0;
-unsigned long current_motor_esc_Millis = 0;
-
-unsigned long previous_motor_esc_Millis = 0;
-
-unsigned long checkMillis = 10000;
-unsigned long check2Millis = 10000;
-unsigned long check3Millis = 10000;
-
-const long buttoninterval = 2000;  // time to hold power switch
-const long boardinterval = 4000;   // time to hold power switch
-
-unsigned long previousMillis = 0;  // will store last time LED was updated
-const long telem = 1000;
-
-int THRR1;
-int THRL1;
-int THRR2;
-int THRL2;
-int THRR3;
-int THRL3;
-
-//wheel RPM
-int RPMR1;
-int RPML1;
-int RPMR2;
-int RPML2;
-int RPMR3;
-int RPML3;
-
-//voltage and temperature
-int VOLT1;
-int TEMP1;
-int VOLT2;
-int TEMP2;
-int VOLT3;
-int TEMP3;
-
-float roll = 0;
-float pitch = 0;
-
-uint8_t idx = 0;         // Index for new data pointer
-uint16_t bufStartFrame;  // Buffer Start Frame
-byte *p;                 // Pointer declaration for the new received data
-byte incomingByte;
-byte incomingBytePrev;
-
-typedef struct {
-  uint16_t start;
-  int16_t steer;
-  int16_t speed;
-  uint16_t checksum;
-} SerialCommand;
-SerialCommand Command;
-
-typedef struct {
-  uint16_t start;
-  int16_t cmd1;
-  int16_t cmd2;
-  int16_t speedR_meas;
-  int16_t speedL_meas;
-  int16_t batVoltage;
-  int16_t boardTemp;
-  uint16_t cmdLed;
-  uint16_t checksum;
-} SerialFeedback;
-SerialFeedback Feedback;
-SerialFeedback NewFeedback;
+  Serial.println(brdallc);
+  int brd1 = digitalRead(8);
+  int brd2 = digitalRead(9);
+  int brd3 = digitalRead(10);
+  Serial.print(" brd1 ");
+  Serial.print(brd1);
+  Serial.print(" brd1 ");
+  Serial.print(brd2);
+  Serial.print(" brd1 ");
+  Serial.println(brd3);
 
 
 
+  if (brdall == 1) {
 
-void setup() {
-  Serial.begin(115200);   // USB
-  Serial1.begin(115200);  // 0,1
-  //Serial2.begin(115200);  // 8,9
+    if (brdallc == 1) {
+      if (brd1 == 1) {
+        digitalWrite(board1switch, LOW);
+      } else {
+        digitalWrite(board1switch, HIGH);
+      }
+      if (brd2 == 1) {
+        digitalWrite(board2switch, LOW);
+      } else {
+        digitalWrite(board2switch, HIGH);
+      }
+      if (brd3 == 1) {
+        digitalWrite(board3switch, LOW);
+      } else {
+        digitalWrite(board3switch, HIGH);
+      }
+      delay(1000);
+      digitalWrite(board1switch, LOW);
+      digitalWrite(board2switch, LOW);
+      digitalWrite(board3switch, LOW);
+      Serial.println("LOW");
+      brdallc = (brdallc + 1);
+      if (brdallc > 1) {
+        brdallc = 10;
+        brdalld = 1;
+      }
+    }
+  }
 
-  HoverSerial1.begin(115200);
-  HoverSerial2.begin(115200);
-  HoverSerial3.begin(115200);
-  //ser6.begin(115200);
 
-  pinMode(board1power, INPUT);
-  pinMode(board2power, INPUT);
-  pinMode(board3power, INPUT);
-
-  pinMode(board1switch, OUTPUT);
-  pinMode(board2switch, OUTPUT);
-  pinMode(board3switch, OUTPUT);
-
-  digitalWrite(board1switch, LOW);
-  digitalWrite(board2switch, LOW);
-  digitalWrite(board3switch, LOW);
-delay(10000);
-  request_Mavlink();
+if (brdall == 0) {
+      if (brdalld == 1) {
+      if (brd1 == 0) {
+        digitalWrite(board1switch, LOW);
+      } else {
+        digitalWrite(board1switch, HIGH);
+      }
+      if (brd2 == 0) {
+        digitalWrite(board2switch, LOW);
+      } else {
+        digitalWrite(board2switch, HIGH);
+      }
+      if (brd3 == 0) {
+        digitalWrite(board3switch, LOW);
+      } else {
+        digitalWrite(board3switch, HIGH);
+      }
+      delay(1000);
+      digitalWrite(board1switch, LOW);
+      digitalWrite(board2switch, LOW);
+      digitalWrite(board3switch, LOW);
+      Serial.println("LOW");
+      brdalld = (brdalld + 1);
+      if (brdalld > 1) {
+        brdalld = 10;
+        brdallc = 1;
+      }
+    }
+  Serial.println("HIGH");
+}
 }
 
 
@@ -154,49 +92,210 @@ delay(10000);
 
 
 
-void loop() {
+void Send1(int16_t uSteer, int16_t uSpeed) {
+  // Create command
+  Command.start = (uint16_t)START_FRAME;
+  Command.steer = (int16_t)uSteer;
+  Command.speed = (int16_t)uSpeed;
+  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
+  HoverSerial1.write((uint8_t *)&Command, sizeof(Command));
+  // Serial.println("send1");
+}
 
-  MavLink_RC();
-  Receive1();
-  Receive2();
-  Receive3();
-  power();
+void Send2(int16_t uSteer, int16_t uSpeed) {
+  // Create command
+  Command.start = (uint16_t)START_FRAME;
+  Command.steer = (int16_t)uSteer;
+  Command.speed = (int16_t)uSpeed;
+  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
+  HoverSerial2.write((uint8_t *)&Command, sizeof(Command));
+  //   Serial.println("send2");
+}
+
+
+void Send3(int16_t uSteer, int16_t uSpeed) {
+  // Create command
+  Command.start = (uint16_t)START_FRAME;
+  Command.steer = (int16_t)uSteer;
+  Command.speed = (int16_t)uSpeed;
+  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
+  HoverSerial3.write((uint8_t *)&Command, sizeof(Command));
+  //  Serial.println("send3");
+}
 
 
 
-  unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= telem) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-    if (digitalRead(board1power)) Mavlink_Telemetry1();
-    if (digitalRead(board2power)) Mavlink_Telemetry2();
-    if (digitalRead(board3power)) Mavlink_Telemetry3();
 
-/*
-    if (digitalRead(board1switch)) Serial.println("1+");
-    if (digitalRead(board2switch)) Serial.println("2+");
-    if (digitalRead(board3switch)) Serial.println("3+");
 
-    if (digitalRead(board1power)) Serial.println("1");
-    if (digitalRead(board2power)) Serial.println("2");
-    if (digitalRead(board3power)) Serial.println("3");
-    Serial.println("             ");
-    */
+
+
+void Receive1() {
+  // Check for new data availability in the Serial buffer
+  if (HoverSerial1.available()) {
+    incomingByte = HoverSerial1.read();                                  // Read the incoming byte
+    bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev;  // Construct the start frame
+  } else {
+    return;
   }
 
+// If DEBUG_RX is defined print all incoming bytes
+#ifdef DEBUG_RX
+  Serial.print(incomingByte);
+  return;
+#endif
+
+  // Copy received data
+  if (bufStartFrame == START_FRAME) {  // Initialize if new data is detected
+    p = (byte *)&NewFeedback;
+    *p++ = incomingBytePrev;
+    *p++ = incomingByte;
+    idx = 2;
+  } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+    *p++ = incomingByte;
+    idx++;
+  }
+
+  // Check if we reached the end of the package
+  if (idx == sizeof(SerialFeedback)) {
+    uint16_t checksum;
+    checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+                          ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+
+    // Check validity of the new data
+    if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+      // Copy the new data
+      memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+      THRR1 = (Feedback.cmd1);
+      THRL1 = (Feedback.cmd2);
+      RPMR1 = (Feedback.speedR_meas);
+      RPML1 = (Feedback.speedL_meas);
+      VOLT1 = (Feedback.batVoltage);
+      TEMP1 = (Feedback.boardTemp);
+      //   Serial.println("VOLT1 ");
+      //  Serial.println(VOLT1);
+
+    } else {
+      // Serial.println("Non-valid data skipped");
+    }
+    idx = 0;  // Reset the index (it prevents to enter in this if condition in the next cycle)
+  }
+
+  // Update previous states
+  incomingBytePrev = incomingByte;
+}
 
 
 
-  //powercycleon();
-  //powercycleoff();
 
+void Receive2() {
 
-  /*TESTING
-  Send1(10, 10);
-    Send2(10, 10);
-      Send3(10, 10);
-         MAVLINK_HB();
-delay (100);
-*/
+  // Check for new data availability in the Serial buffer
+  if (HoverSerial2.available()) {
+    incomingByte = HoverSerial2.read();                                  // Read the incoming byte
+    bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev;  // Construct the start frame
+  } else {
+    return;
+  }
+
+// If DEBUG_RX is defined print all incoming bytes
+#ifdef DEBUG_RX
+  Serial.print(incomingByte);
+  return;
+#endif
+
+  // Copy received data
+  if (bufStartFrame == START_FRAME) {  // Initialize if new data is detected
+    p = (byte *)&NewFeedback;
+    *p++ = incomingBytePrev;
+    *p++ = incomingByte;
+    idx = 2;
+  } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+    *p++ = incomingByte;
+    idx++;
+  }
+
+  // Check if we reached the end of the package
+  if (idx == sizeof(SerialFeedback)) {
+    uint16_t checksum;
+    checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+                          ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+
+    // Check validity of the new data
+    if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+      // Copy the new data
+      memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+      THRR2 = (Feedback.cmd1);
+      THRL2 = (Feedback.cmd2);
+      RPMR2 = (Feedback.speedR_meas);
+      RPML2 = (Feedback.speedL_meas);
+      VOLT2 = (Feedback.batVoltage);
+      TEMP2 = (Feedback.boardTemp);
+      //         Serial.println("VOLT2 ");
+      //   Serial.println(VOLT2);
+
+      // Print data to built-in Serial
+
+    } else {
+      //  Serial.println("Non-valid data skipped");
+    }
+    idx = 0;  // Reset the index (it prevents to enter in this if condition in the next cycle)
+  }
+
+  // Update previous states
+  incomingBytePrev = incomingByte;
+}
+
+void Receive3() {
+  // Check for new data availability in the Serial buffer
+  if (HoverSerial3.available()) {
+    incomingByte = HoverSerial3.read();                                  // Read the incoming byte
+    bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev;  // Construct the start frame
+  } else {
+    return;
+  }
+
+// If DEBUG_RX is defined print all incoming bytes
+#ifdef DEBUG_RX
+  Serial.print(incomingByte);
+  return;
+#endif
+
+  // Copy received data
+  if (bufStartFrame == START_FRAME) {  // Initialize if new data is detected
+    p = (byte *)&NewFeedback;
+    *p++ = incomingBytePrev;
+    *p++ = incomingByte;
+    idx = 2;
+  } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+    *p++ = incomingByte;
+    idx++;
+  }
+
+  // Check if we reached the end of the package
+  if (idx == sizeof(SerialFeedback)) {
+    uint16_t checksum;
+    checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+                          ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+
+    // Check validity of the new data
+    if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+      // Copy the new data
+      memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+      THRR3 = (Feedback.cmd1);
+      THRL3 = (Feedback.cmd2);
+      RPMR3 = (Feedback.speedR_meas);
+      RPML3 = (Feedback.speedL_meas);
+      VOLT3 = (Feedback.batVoltage);
+      TEMP3 = (Feedback.boardTemp);
+      //    Serial.println("VOLT3 ");
+      //   Serial.println(VOLT3);
+    } else {
+      //Serial.println("Non-valid data skipped");
+    }
+    idx = 0;  // Reset the index (it prevents to enter in this if condition in the next cycle)
+  }
+
+  // Update previous states
+  incomingBytePrev = incomingByte;
 }
